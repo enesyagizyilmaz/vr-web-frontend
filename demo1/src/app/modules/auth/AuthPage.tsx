ab2abde7c23da7a15557dd71ebe669226f7428b6
enesyagizyilmaz <enesy2lmz@icloud.com>
2023-11-14 20:11:54 +0300

    signup work

--- demo1/src/app/modules/auth/AuthPage.tsx
+++ demo1/src/app/modules/auth/AuthPage.tsx
@@ -1,14 +1,15 @@
 import {Route, Routes} from 'react-router-dom'
-import {Registration} from './components/Registration'
+import {SignUp} from './components/SignUp'
 import {ForgotPassword} from './components/ForgotPassword'
 import {Login} from './components/Login'
 import {AuthLayout} from './AuthLayout'
+import React from "react";
 
 const AuthPage = () => (
   <Routes>
     <Route element={<AuthLayout />}>
       <Route path='login' element={<Login />} />
-      <Route path='registration' element={<Registration />} />
+      <Route path='registration' element={<SignUp />} />
       <Route path='forgot-password' element={<ForgotPassword />} />
       <Route index element={<Login />} />
     </Route>
