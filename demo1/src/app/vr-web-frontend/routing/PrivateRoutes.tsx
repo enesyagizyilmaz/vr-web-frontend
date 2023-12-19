c6dff406d6a1dcaf4e073a43b1593fce6ef64709
enesyagizyilmaz <enesy2lmz@icloud.com>
2023-12-19 23:25:42 +0300

    signup work

--- demo1/src/app/vr-web-frontend/routing/PrivateRoutes.tsx
+++ demo1/src/app/vr-web-frontend/routing/PrivateRoutes.tsx
@@ -1,31 +1,28 @@
-import {lazy, FC, Suspense} from 'react'
+import React, {lazy, FC, Suspense} from 'react'
 import {Route, Routes, Navigate} from 'react-router-dom'
-import {MasterLayout} from '../../_metronic/layout/MasterLayout'
+import {MasterLayout} from '../../../_metronic/layout/MasterLayout'
 import TopBarProgress from 'react-topbar-progress-indicator'
-import {DashboardWrapper} from '../pages/dashboard/DashboardWrapper'
-import {MenuTestPage} from '../pages/MenuTestPage'
-import {getCSSVariableValue} from '../../_metronic/assets/ts/_utils'
-import {WithChildren} from '../../_metronic/helpers'
-import BuilderPageWrapper from '../pages/layout-builder/BuilderPageWrapper'
+import {DashboardWrapper} from '../../pages/dashboard/DashboardWrapper'
+import {MenuTestPage} from '../../pages/MenuTestPage'
+import {getCSSVariableValue} from '../../../_metronic/assets/ts/_utils'
+import {WithChildren} from '../../../_metronic/helpers'
+import BuilderPageWrapper from '../../pages/layout-builder/BuilderPageWrapper'
 
 const PrivateRoutes = () => {
-  const ProfilePage = lazy(() => import('../modules/profile/ProfilePage'))
-  const WizardsPage = lazy(() => import('../modules/wizards/WizardsPage'))
-  const AccountPage = lazy(() => import('../modules/accounts/AccountPage'))
-  const WidgetsPage = lazy(() => import('../modules/widgets/WidgetsPage'))
-  const ChatPage = lazy(() => import('../modules/apps/chat/ChatPage'))
-  const UsersPage = lazy(() => import('../modules/apps/user-management/UsersPage'))
+  const ProfilePage = lazy(() => import('../../modules/profile/ProfilePage'))
+  const WizardsPage = lazy(() => import('../../modules/wizards/WizardsPage'))
+  const AccountPage = lazy(() => import('../../modules/accounts/AccountPage'))
+  const WidgetsPage = lazy(() => import('../../modules/widgets/WidgetsPage'))
+  const ChatPage = lazy(() => import('../../modules/apps/chat/ChatPage'))
+  const UsersPage = lazy(() => import('../../modules/apps/user-management/UsersPage'))
 
   return (
     <Routes>
       <Route element={<MasterLayout />}>
-        {/* Redirect to Dashboard after success login/registartion */}
         <Route path='auth/*' element={<Navigate to='/dashboard' />} />
-        {/* Pages */}
         <Route path='dashboard' element={<DashboardWrapper />} />
         <Route path='builder' element={<BuilderPageWrapper />} />
         <Route path='menu-test' element={<MenuTestPage />} />
-        {/* Lazy Modules */}
         <Route
           path='crafted/pages/profile/*'
           element={
@@ -74,7 +71,6 @@ const PrivateRoutes = () => {
             </SuspensedView>
           }
         />
-        {/* Page Not Found */}
         <Route path='*' element={<Navigate to='/error/404' />} />
       </Route>
     </Routes>
