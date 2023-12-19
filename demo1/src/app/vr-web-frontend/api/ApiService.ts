196328868d542ae90c36d0f6182b766307cbaec3
enesyagizyilmaz <enesy2lmz@icloud.com>
2023-12-19 23:28:13 +0300

    signup work

---
+++ demo1/src/app/vr-web-frontend/api/ApiService.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const apiClient = axios.create
+(
+    {
+        baseURL: 'http://localhost:8080'
+    }
+);
+
+export const signUpApi = (body) =>
+{
+    return apiClient.post(`/api/v1/users`,body);
+};
+
+export const login = (credentials) => {
+  return apiClient.post("/api/v1/auth",credentials);
+}
