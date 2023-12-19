398751f730fbfd4a64d395e05a05b35089e8204e
enesyagizyilmaz <enesy2lmz@icloud.com>
2023-12-19 23:17:53 +0300

    signup work

---
+++ demo1/src/app/vr-web-frontend/App.tsx
@@ -0,0 +1,22 @@
+import {Suspense} from 'react'
+import {Outlet} from 'react-router-dom'
+import {I18nProvider} from '../../_metronic/i18n/i18nProvider'
+import {LayoutProvider, LayoutSplashScreen} from '../../_metronic/layout/core'
+import {MasterInit} from '../../_metronic/layout/MasterInit'
+import {AuthInit} from '../modules/auth'
+import {ThemeModeProvider} from '../../_metronic/partials'
+
+const App = () => {
+  return (
+    <Suspense fallback={<LayoutSplashScreen />}>
+        <LayoutProvider>
+          <ThemeModeProvider>
+              <Outlet />
+              <MasterInit />
+          </ThemeModeProvider>
+        </LayoutProvider>
+    </Suspense>
+  )
+}
+
+export {App}
