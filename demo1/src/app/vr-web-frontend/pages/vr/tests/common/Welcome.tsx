af5ccb0f98fb695cf1f3da3fa0ca52e5e250445b
enesyagizyilmaz <enesy2lmz@icloud.com>
2024-01-02 21:45:50 +0300

    site modified

--- demo1/src/app/vr-web-frontend/pages/vr/tests/common/Welcome.tsx
+++ demo1/src/app/vr-web-frontend/pages/vr/tests/common/Welcome.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 interface TestIntroProps {
     handleShowQuestion: () => void;
@@ -10,6 +11,11 @@ const Welcome: React.FC<TestIntroProps> = ({ handleShowQuestion, testTitle}) => 
         <div className="card shadow-sm">
             <div className="card-header">
                 <h1 className="card-title">{testTitle}</h1>
+                <div className={"card-title"}>
+                    <Link to='/dashboard' className='btn btn-sm btn-primary'>
+                        Return Home
+                    </Link>
+                </div>
             </div>
             <div className="card-body">
                 <h3>There are 5 questions in this test. If you are ready, click the button to take the test</h3>
